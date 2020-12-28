a17e41beafc04ca97e07471c5dc116fef1d66d92
Shahaab Manzar <mshahaabmanzar@gmail.com>
2020-12-28 19:05:43 +0530

    commit

--- app.js
+++ app.js
@@ -3,6 +3,14 @@ const bodyParser=require("body-parser");
 const request= require("request");
 
 const app= express();
+
+app.use(express.static("public"));
+
+app.get("/", function(req,res){
+    res.sendFile(__dirname+"/signup.html")
+});
+
+
 app.listen(3000, function(){
     console.log("server is running on port 3000");
 });
